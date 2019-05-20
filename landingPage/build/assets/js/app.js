6c41ecd1f96ee1b497dca47653f9945f28b2574c
Shiraz Chokshi <shirazchokshi@Shirazs-MacBook-Pro.local>
2019-05-20 03:30:03 -0400

    changed to gh-pages

--- landingPage/build/assets/js/app.js
+++ landingPage/build/assets/js/app.js
@@ -1,1 +1,1 @@
-window.sr=ScrollReveal();
+function showNav(){console.log("ayyy lmao");var e=document.getElementById("responsive-nav");"responsive-nav"===e.className?e.className+=" unfold":e.className="responsive-nav"}window.sr=ScrollReveal();
--- landingPage/source/js/app.js
+++ landingPage/source/js/app.js
@@ -1,10 +1,23 @@
 
 // JavaScript
-window.sr = ScrollReveal(); 
+window.sr = ScrollReveal();
 
 // sr.reveal('h1', {
 //     delay: 0,
 //     duration: 200,
 //     origin: 'bottom',
-//     distance: '100px' 
+//     distance: '100px'
 // });
+
+function showNav(){
+  console.log("ayyy lmao");
+  var x = document.getElementById("responsive-nav");
+  if(x.className === "responsive-nav"){
+    x.className += " unfold";
+
+  }
+  else{
+    x.className = "responsive-nav"
+  }
+
+}
